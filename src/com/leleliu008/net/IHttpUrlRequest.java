421b5c7fbe179ad6d3ebde146dfa005431a33c3b
leleliu008 <792793182@qq.com>
2014-09-05 06:35:04 +0800

    删除无用的代码

--- src/com/leleliu008/net/IHttpUrlRequest.java
+++
@@ -1,9 +0,0 @@
-package com.leleliu008.net;
-
-import java.io.IOException;
-
-/* 使用Http + url协议请求网络连接 */
-interface IHttpUrlRequest {
-	
-	byte[] request(String urlStr) throws IOException;
-}
--- src/com/leleliu008/net/RequestMethod.java
+++
@@ -1,7 +0,0 @@
-package com.leleliu008.net;
-
-/* 请求网络的方式 */
-public enum RequestMethod {
-	HttpUrlConnection, /* JDK中的HttpUrlConnection */
-	HttpClient         /* Apache的HttpClient */
-}
